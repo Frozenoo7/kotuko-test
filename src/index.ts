2154a6a93e132ff938c000840a2bc19ff64b051e
kiran <kiran@pop-os.localdomain>
2023-11-23 15:42:21 +0545

    database connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import express from "express";
 
+import { AppDataSource } from "database/database";
+
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,15 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.APP_PORT || 3000;
+
+// establish database connection
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
 
 app.listen(PORT, async () => {
   console.log(`listning on port ${PORT}`);
