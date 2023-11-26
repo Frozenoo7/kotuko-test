21787024f3b2e5f417ec4b6cd6982a5f09981b7d
kiran <kiran@pop-os.localdomain>
2023-11-26 10:11:34 +0545

    -sample of env in env.example file

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,6 +2,7 @@ import { createLogger, format, transports } from "winston";
 
 const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
+  /**Later on this logs can be integrated to log mangement tools */
   transports: [
     new transports.File({
       filename: "app-error.log",
